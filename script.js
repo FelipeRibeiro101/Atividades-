cb26f2c4c0c1f3f52cc83bb08e54af4eb4d78f79
FelipeRibeiro101 <lipe.menna@gmail.com>
2024-08-16 11:10:52 -0300

    Aula
    Aula do dia 16/08/2024

---
+++ script.js
@@ -0,0 +1,13 @@
+// perguntando ao usuario
+let nome = prompt ("Digite seu nome");
+alert("ola "+nome", tudo bem?");
+
+// alert na tela
+alert("Ola mundo!");
+
+// console do navegador
+console.log ("teste")
+
+
+// inclui um html em um elemento identificado pelo IS
+document.getElementById("demo").innerHTML = "Felipe Ribeiro";
